955e950cd93c51195dcdae752c25229bf9ac3798
icaev <icaevvlad81@gmail.com>
2022-07-26 14:55:45 +0300

    Updated(added third lesson)

---
+++ third/main.py
@@ -0,0 +1,7 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+
+TOKEN = "5596751604:AAE-kA_Mvso9liG6hqmxmL3JxhyjazfqPuY"
+bot = Bot(token=TOKEN, parse_mode='html')
+dp = Dispatcher(bot=bot)
+
